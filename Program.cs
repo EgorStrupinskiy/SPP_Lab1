06048a8b5ceaa82755913de978dba85ffb8ecc5f
Venik2405 <venik2405@yandex.ru>
2022-10-12 21:55:03 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lab1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
